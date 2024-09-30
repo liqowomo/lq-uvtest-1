623b63e76630817b3c68b56a2f9bb8964e050f44
liqowomo <123171512+liqowomo@users.noreply.github.com>
2024-09-30 04:57:29 +0000

    🫦

--- gfx/test.sh
+++ gfx/test.sh
@@ -1,17 +1,36 @@
 #!/usr/bin/env sh
 
 main() {
-  test
   test2
 }
 
-test() {
-     curl https://snips.sh/f/sNTH05AZbk
-}
+# test() {
+#      curl https://snips.sh/f/sNTH05AZbk
+# }
 
 test2() {
   output=$(curl -s https://snips.sh/f/sNTH05AZbk)
-  printf "%s %s\n" "$output" "Your additional text here"
+  printf "%s %s" "$output" "Your additional text here"
+  printf "
+                bondo fondats 
+  "
 }
 
+# test3() {
+#   output=$(curl -s https://snips.sh/f/sNTH05AZbk)
+#   COLUMNS=$(tput cols)
+#   lines=()
+#   while IFS= read -r line; do
+#     lines+=("$line")
+#   done <<< "$output"
+
+#   for line in "${lines[@]}"; do
+#     printf "%*s%s\n" $(( (COLUMNS - ${#line}) / 2 )) "" "$line"
+#   done
+
+#   echo "Your additional text here"
+# }
+
+
+# need this main function 
 main
