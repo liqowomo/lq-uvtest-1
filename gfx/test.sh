9c3c5e13d4efa47cdf32fb1c0c42248e94d2d476
liqowomo <123171512+liqowomo@users.noreply.github.com>
2024-09-30 04:42:36 +0000

    🫦

--- gfx/test.sh
+++ gfx/test.sh
@@ -2,10 +2,16 @@
 
 main() {
   test
+  test2
 }
 
 test() {
      curl https://snips.sh/f/sNTH05AZbk
+}
+
+test2() {
+  output=$(curl -s https://snips.sh/f/sNTH05AZbk)
+  printf "%s %s\n" "$output" "Your additional text here"
 }
 
 main
