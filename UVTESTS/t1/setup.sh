bbc660652580a5c45f9bf6b9578995bac6f56fac
liqowomo <123171512+liqowomo@users.noreply.github.com>
2024-09-30 03:44:08 +0000

    🫦

---
+++ UVTESTS/t1/setup.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh 
+
+# Setting the color varibales 
+Cyan="\033[36m"
+Magenta="\033[35m"
+
+# Check for command
+if ! command -v uv &> /dev/null; then
+    echo "\033[36mInstalling uv\033[0m" # Using ANSI escape codes for color
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    uv --version
+fi
+echo ""
+echo ""
+echo  "${Magenta}==================="
+echo  " Testing out the installs of UV Related"
+echo  " 1. uv add textual pytest-playwright"
+echo  " 2. uvx playwright install"
+echo  ""
+echo  "===================${Cyan}"
+sudo playwright install-deps 
+uv add textual pytest-playwright
+sudo playwright install-deps 
+uvx playwright install
+uv tree 
